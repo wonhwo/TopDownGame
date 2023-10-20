54690d783a5a82ef8990bfa419e6ee2e3e5869f0
hyowon <dlgy9714@gmail.com>
2023-10-21 07:23:03 +0900

    왜 두번하게하냐

---
+++ TopDownGame/Assets/Scenes/Script/LevelManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject childObject; // Inspector에서 자식 오브젝트를 연결
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+        }
+    }
+}
